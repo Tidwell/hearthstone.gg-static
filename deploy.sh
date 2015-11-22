e08dbfbced4c72716f26ceb83f9737165bab9a7a
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-11-22 04:11:12 -0500

    more

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ then
 	echo 'Git okay, ready to deploy';
 else
 	echo 'Files not commited to git.  Do that before deploy.';
+	exit 1;
 fi
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
