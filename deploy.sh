31c58df638c7bf6d0ebaf9c2013e06422d5430ac
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-11-22 04:10:00 -0500

    more

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 status=`git status`;
 echo ${status};
+if [ ${status} == 'On branch deploy-git-check nothing to commit, working directory clean']
+then
+	echo 'Git okay, ready to deploy';
+else
+	echo 'Files not commited to git.  Do that before deploy.';
+fi
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
