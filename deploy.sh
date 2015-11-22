1385ea58a92970ebe7a22731a4beb60785912ea9
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-11-22 04:21:36 -0500

    check pushed

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@
 if [ -n "$(git status --porcelain)" ]; then 
 	echo 'Files not commited to git.  Do that before deploy.';
 	exit 1;
- fi
+fi
+if [ -n "$(git diff origin/master..HEAD)"]; then
+	echo 'Files not pushed upstream via git.  Do that before deploy.'
+	exit 1;
+fi
 
 read -p "Are you sure you want to deploy?  This will replace *everything* on the server. (y/n) " -n 1 -r
 echo    # (optional) move to a new line
