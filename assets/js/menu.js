069916b2699fc575530506aae7a567583814c5b9
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-11-21 23:50:57 -0500

    sidebar menu styles and js for transitions

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,28 @@
 (function($) {
+	var $sidebar;
+	var $menu;
+	var $menuTrigger;
+
+	function getElements() {
+		$sidebar = $('[data-sidebar]');
+		$menu = $('[data-menu]');
+		$menuTrigger = $('[data-menu-trigger]');
+	}
+
+	function bindDOM() {
+		$sidebar.addClass('js-active');
+		$sidebar.on('click', '[data-menu-trigger]', toggleMenu);
+		
+	}
+
+	function toggleMenu() {
+		$sidebar.toggleClass('menu-open');
+		return false;
+	}
 
 	function init() {
-		
+		getElements();
+		bindDOM();
 	}
 
 	$(init);
