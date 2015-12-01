a8a5931d0af132f604df17678d7bc1f4792f4c5a
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-11-30 19:50:47 -0500

    put comments behind click

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,14 +1,27 @@
-(function() {
+(function($) {
 	if (typeof disqus_config === 'undefined' || !disqus_config) { return; }
-	
-	//https://hearthstonegg.disqus.com/admin/settings/universalcode/	
-	(function() { // DON'T EDIT BELOW THIS LINE
-	var d = document, s = d.createElement('script');
 
-	s.src = '//hearthstonegg.disqus.com/embed.js';
+	var $showCommentsButton;
 
-	s.setAttribute('data-timestamp', +new Date());
-	(d.head || d.body).appendChild(s);
-	})();
+	function show() {
+		$showCommentsButton.unbind().remove();
 
-}());
+		//https://hearthstonegg.disqus.com/admin/settings/universalcode/	
+		(function() { // DON'T EDIT BELOW THIS LINE
+		var d = document, s = d.createElement('script');
+
+		s.src = '//hearthstonegg.disqus.com/embed.js';
+
+		s.setAttribute('data-timestamp', +new Date());
+		(d.head || d.body).appendChild(s);
+		})();
+
+	}
+	function init() {
+		$showCommentsButton = $('[data-show-comments]').show();
+		$showCommentsButton.click(show);
+	}
+
+	$(init);
+
+}(jQuery));
