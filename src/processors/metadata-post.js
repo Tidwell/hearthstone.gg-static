1afdee7db58aa945e6e5325ce88013cf1dc27949
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-12-05 16:29:59 -0500

    move webPath creation to static-site-generator

--- src/processors/metadata-post.js
+++ src/processors/metadata-post.js
@@ -12,8 +12,6 @@ module.exports = function metadataPostProcessor(writeFilesProcessor) {
 				var metadata = doc.metadata;
 				//format a date as a js date object
 				if (metadata.date) { metadata.date = new Date(Number(metadata.date)); }
-				//add a .webPath property relative to the build root
-				doc.webPath = doc.outputPath.replace(writeFilesProcessor.outputFolder, '');
 			});
 		}
 	};
